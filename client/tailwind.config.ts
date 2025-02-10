dbb98b9fe58045331321a66b2ab4bb01b4906590
princedelatrojan <princetrojan@proton.me>
2025-02-10 12:11:32 +0300

    initial commit

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,14 +3,14 @@ import { createThemes } from "tw-colors";
 import colors from "tailwindcss/colors";
 
 const baseColors = [
-    "gray",
-    "red",
-    "green",
-    "yellow",
-    "blue",
-    "indigo",
-    "purple",
-    "pink",
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
 ];
 
 const shadeMapping = {
@@ -26,17 +26,17 @@ const shadeMapping = {
   "900": "50",
 };
 
-const generateThemeObject = (colors: any, mapping:any, invert = false) => {
+const generateThemeObject = (colors: any, mapping: any, invert = false) => {
   const theme: any = {};
   baseColors.forEach((color) => {
     theme[color] = {};
-    Object.entries(mapping).forEach(([key, value] : any) => {
+    Object.entries(mapping).forEach(([key, value]: any) => {
       const shadeKey = invert ? value : key;
       theme[color][key] = colors[color][shadeKey];
     });
   });
-    return theme;
-}
+  return theme;
+};
 
 const lightTheme = generateThemeObject(colors, shadeMapping);
 const darkTheme = generateThemeObject(colors, shadeMapping, true);
@@ -44,16 +44,16 @@ const darkTheme = generateThemeObject(colors, shadeMapping, true);
 const themes = {
   light: {
     ...lightTheme,
-    white: "#ffffff"
+    white: "#ffffff",
   },
   dark: {
     ...darkTheme,
     white: colors.gray["950"],
-    black: colors.gray["50"]
-  }
-}
+    black: colors.gray["50"],
+  },
+};
 
-export default {
+const config: Config = {
   darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -62,12 +62,14 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [createThemes(themes)],
+};
 
-} satisfies Config;
+export default config;
--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -4,10 +4,10 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const users = await prisma.users.findMany();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving users" });
-    }
+  try {
+    const users = await prisma.users.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving users" });
+  }
 };
